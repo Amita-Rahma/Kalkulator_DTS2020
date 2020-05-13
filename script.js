6b7fbcef1d8764957b1731c1ee88a10088a8c399
Amita-Rahma <64511939+Amita-Rahma@users.noreply.github.com>
2020-05-13 14:13:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const numbers = document.querySelectorAll(".number");
+const numbers = document.querySelectorAll(".number")
 
 numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-   console.log ("number is pressed")
+  number.addEventListener("click", (event) => {
+   console.log (event.target.value)
   })
-});
+})
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+ calculatorScreen.value = number
+}
