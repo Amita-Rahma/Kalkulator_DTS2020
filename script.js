f62f6592719568b4b6c23a4d1b1364d995732c73
Amita-Rahma <64511939+Amita-Rahma@users.noreply.github.com>
2020-05-13 13:53:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const numbers = document.querySelectorAll(".number");
+
 numbers.forEach ((number) => {
   number.addEventListener("click", () => {
-   console.log("number is pressed);
+   console.log("number is pressed");
   });
 });
