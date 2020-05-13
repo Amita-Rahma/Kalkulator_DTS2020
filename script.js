87f1958c2a67aa5b1a20e6d326a5a9168a874ace
Amita-Rahma <64511939+Amita-Rahma@users.noreply.github.com>
2020-05-13 13:16:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const numbers = document.querySelectorAll(".number")
+console.log(numbers)
+
+const numbers = document.querySelectorAll(".number")
 numbers.forEach ((number) => {
   number.addEventListener("click", (event) => {
    console.log(event.target.value)
