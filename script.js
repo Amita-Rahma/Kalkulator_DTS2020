2be4f1ec5deb40871a14aad862125ee5784c08e5
Amita-Rahma <64511939+Amita-Rahma@users.noreply.github.com>
2020-05-16 14:11:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,61 +26,8 @@ operators.forEach((operator)=>{
     inputOperator(event.target.value)
   })
 })
-
 const inputOperator=(operator)=>{
- if (calculationOperator ==='') {
   prevNumber=currentNumber
- }
   calculationOperator=operator
-  currentNumber='0'
+  currentNumber=''
 }
-
-const calculate = () => {
- let result = ''
- switch(calculationOperator) {
-  case "+" :
-   result = parseFloat(prevNumber)+parseFloat(currentNumber)
-   break
-  case "-" :
-   result = prevNumber-currentNumber
-   break
-  case "*" :
-   result = prevNumber*currentNumber
-   break
-  case "/" :
-   result = prevNumber/currentNumber
-   break
-  default:
-   return
- }
- currentNumber = result
- calculationOperation = ''
-}
-
-const equalSign = document.querySelector('.equal-sign')
-equalSign.addEventListener('click', () => {
-  calculate ()
-  updateScreen (currentNumber)
-})
-
-const clearAll = () => {
- prevNumber = ''
- calculationOperator = ''
- currentNumber = '0'
-}
-
-const clearBtn = document.querySelector('.all-clear')
-clearBtn.addEventListener('click', () => {
-  clearAll()
-  updateScreen(currentNumber)
-})
-
-inputDecimal = (dot) => {
- currentNumber += dot
-}
-
-const decimal = document.querySelector('.decimal')
-decimal.addEventListener('click', (event) => {
- inputDecimal (event.target.value)
- updateScreen(currentNumber)
-})
